04859d7238d9cb4cc888c6f01df78981ad993c25
titi0267 <timothe@coniel.com>
2022-01-05 18:46:28 +0100

    task 18

--- My.hs
+++ My.hs
@@ -77,3 +77,7 @@ myDrop y (x:xs) = myDrop (y-1) xs
 myAppend :: [ a ] -> [ a ] -> [ a ]
 myAppend [] x = x
 myAppend (x:xs) y = x : myAppend xs y
+
+myReverse :: [ a ] -> [ a ]
+myReverse [] = []
+myReverse (x:xs) = myAppend (myReverse xs) [x]
