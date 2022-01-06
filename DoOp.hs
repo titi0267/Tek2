51f0e8e79599572285b2202930a47ba2ab682847
titi0267 <timothe@coniel.com>
2022-01-06 10:51:24 +0100

    task 3

--- DoOp.hs
+++ DoOp.hs
@@ -10,3 +10,20 @@ myElem a (x:xs)
 safeDiv :: Int -> Int -> Maybe Int
 safeDiv a 0 = Nothing
 safeDiv a b = Just (a `div` b)
+
+myLength :: [ a ] -> Int
+myLength [] = 0
+myLength (_:xs) = 1 + myLength xs
+
+myIsNeg :: Int -> Bool
+myIsNeg x
+    | x < 0 = True 
+    | otherwise = False
+
+safeNth :: [ a ] -> Int -> Maybe a
+safeNth [] y = Nothing
+safeNth (x:xs) y
+    | y > myLength (x:xs) = Nothing
+    | myIsNeg y = Nothing
+safeNth (x:xs) 0 = Just x
+safeNth (x:xs) y = safeNth xs (y-1)
