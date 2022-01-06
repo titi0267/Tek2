a75ebec39f97bc8f81042052eeaac04d99c1c10e
titi0267 <timothe@coniel.com>
2022-01-06 10:46:47 +0100

    task 2

--- DoOp.hs
+++ DoOp.hs
@@ -1,5 +1,12 @@
+import Data.Maybe ()
+
 myElem :: Eq a => a -> [ a ] -> Bool
 myElem a [] = False
 myElem a (x:xs)
     | a == x = True 
     | otherwise = myElem a xs
+
+
+safeDiv :: Int -> Int -> Maybe Int
+safeDiv a 0 = Nothing
+safeDiv a b = Just (a `div` b)
