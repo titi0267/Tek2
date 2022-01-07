8aceb7cca9c22e29913b916b956043dfdd51d374
titi0267 <timothe@coniel.com>
2022-01-07 16:06:39 +0100

    task 12 & 13

--- Tree.hs
+++ Tree.hs
@@ -1,5 +1,12 @@
-import Data.IntMap (showTree)
 data Tree a = Empty | Node (Tree a) a (Tree a)
     deriving (Show)
 
+addInTree :: Ord a => a -> Tree a -> Tree a
+addInTree a Empty = (Node Empty a Empty)
+addInTree a (Node left b right) =  case a < b of
+    True -> (Node (addInTree a left) b right)
+    False -> (Node left b (addInTree a right))
+
+class Functor f where
+    fmap :: f -> Tree a -> Tree a
 
