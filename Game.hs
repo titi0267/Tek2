5a862b536d4da64932951d8a984a9b491ffc2fb5
titi0267 <timothe@coniel.com>
2022-01-07 13:02:30 +0100

    Fix taks 5-8

--- Game.hs
+++ Game.hs
@@ -4,7 +4,7 @@ data Item = Sword | Bow | MagicWand
 instance Show Item where
     show = showItem
 
-data Mob = Skeleton (Maybe Item) | Witch (Maybe Item) | Mummy
+data Mob = Skeleton Item | Witch (Maybe Item) | Mummy
     deriving (Eq)
 
 instance Show Mob where
@@ -20,10 +20,10 @@ createMummy :: Mob -- a Mummy
 createMummy = Mummy
 
 createArcher :: Mob -- a Skeleton holding a Bow
-createArcher = Skeleton (Just Bow)
+createArcher = Skeleton Bow
 
 createKnight :: Mob -- a Skeleton holding a Sword
-createKnight = Skeleton (Just Sword)
+createKnight = Skeleton Sword
 
 createWitch :: Mob -- a Which holding Nothing
 createWitch = Witch Nothing
@@ -33,24 +33,24 @@ createSorceress = Witch (Just MagicWand)
 
 showMob :: Mob -> String
 showMob Mummy = "mummy"
-showMob (Skeleton (Just Bow)) = "doomed archer"
-showMob (Skeleton (Just Sword)) = "dead knight"
-showMob (Skeleton (Just MagicWand)) = "skeleton holding a " ++ showItem MagicWand
+showMob (Skeleton Bow) = "doomed archer"
+showMob (Skeleton Sword) = "dead knight"
+showMob (Skeleton MagicWand) = "skeleton holding a " ++ showItem MagicWand
 showMob (Witch Nothing) = "witch"
 showMob (Witch (Just MagicWand)) = "sorceress"
-showMob (Witch (Just Sword)) = "witch holding a Sword"
+showMob (Witch (Just Sword)) = "witch holding a " ++ showItem Sword
 showMob _ = ""
 
 create :: String -> Maybe Mob
 create "mummy" = Just Mummy
-create "doomed archer" = Just $ Skeleton (Just Bow)
-create "dead knight" = Just $ Skeleton (Just Sword)
+create "doomed archer" = Just $ Skeleton Bow
+create "dead knight" = Just $ Skeleton Sword
 create "witch" = Just $ Witch Nothing 
 create "sorceress" = Just $ Witch (Just MagicWand)
 create _ = Nothing
 
 equip :: Item -> Mob -> Maybe Mob
 equip a Mummy = Just Mummy
-equip a (Skeleton (Just _))= Just $ Skeleton (Just a)
-equip a (Witch (Just _)) = Just $ Witch (Just a)
-equip a _ = Nothing
+equip a (Skeleton b)= Just $ Skeleton a
+equip a (Witch (Just b)) = Just $ Witch (Just a)
+equip a _ = Just $ Witch (Just a)
