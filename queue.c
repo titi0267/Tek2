b1178b550f3a615f0ad4ad542d8a12c6530d5726
titi0267 <timothe@coniel.com>
2022-01-18 14:31:44 +0100

    fix

--- queue.c
+++ queue.c
@@ -39,30 +39,3 @@ void *queue_front(queue_t queue)
     return (list_get_elem_at_back(queue));
 }
 
-int main ( void )
-{
-queue_t queue = NULL ;
-int i = 5;
-int j = 4;
-int * data = NULL ;
-queue_push (& queue , & i ) ;
-queue_push (& queue , & j ) ;
-data = ( int *) queue_front ( queue ) ;
-printf ("%d\n", * data ) ;
-queue_clear (& queue ) ;
-return (0) ;
-}
-/*
-int main ( void )
-{
-queue_t queue = NULL ;
-int i = 5;
-int j = 4;
-int * data = NULL;
-queue_push(&queue, &i);
-queue_push(&queue, &j);
-data = (int *)queue_front(queue);
-printf("%d\n", *data);
-queue_clear (&queue);
-return (0);
-}*/
