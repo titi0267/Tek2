2f16b1c61cf7de635451270946046cc6283aa111
titi0267 <timothe@coniel.com>
2022-01-26 21:33:39 +0100

    fix std exeption

--- ex00/Errors.cpp
+++ ex00/Errors.cpp
@@ -8,32 +8,31 @@
 #include "Errors.hpp"
 #include <exception>
 
-const char *NasaError::what() const throw()
+const char *NasaError::what() const noexcept
 {
     return _message.c_str();
 }
 
-NasaError::NasaError(std::string const &message, std::string const &component) throw()
+NasaError::NasaError(std::string const &message, std::string const &component)
 {
     _message = message;
     _component = component;
 }
 
-MissionCriticalError::MissionCriticalError(std::string const &message, std::string const &component) throw() : NasaError(message, component)
+MissionCriticalError::MissionCriticalError(std::string const &message, std::string const &component) : NasaError(message, component)
 {
 }
 
-LifeCriticalError::LifeCriticalError(std::string const &message, std::string const &component) throw() : NasaError(message, component)
+LifeCriticalError::LifeCriticalError(std::string const &message, std::string const &component) : NasaError(message, component)
 {
 }
 
-UserError::UserError(std::string const &message, std::string const &component) throw() : NasaError(message, component)
+UserError::UserError(std::string const &message, std::string const &component) : NasaError(message, component)
 {
 }
 
-CommunicationError::CommunicationError(std::string const &message, std::string const &component) throw() : NasaError(message, component)
+CommunicationError::CommunicationError(std::string const &message) : NasaError(message, "CommunicationDevice")
 {
-    _component = "CommunicationDevice";
 }
 
 std::string const &NasaError::getComponent() const
--- ex00/Errors.hpp
+++ ex00/Errors.hpp
@@ -4,13 +4,12 @@
 #include <string>
 #include <exception>
 
-class NasaError : std::exception
+class NasaError : public std::exception
 {
     public:
-        NasaError(std::string const &message, std::string const &component = "Unknown") throw();
+        NasaError(std::string const &message, std::string const &component = "Unknown");
         std::string const &getComponent() const;
-        virtual const char* what() const throw();
-    //virtual ~Erreur() throw();
+        virtual const char* what() const noexcept;
 
     protected:
         std::string _message;
@@ -21,19 +20,19 @@ class NasaError : std::exception
 class MissionCriticalError : public NasaError
 {
     public:
-        MissionCriticalError(std::string const &message, std::string const &component = "Unknown") throw();
+        MissionCriticalError(std::string const &message, std::string const &component = "Unknown");
 };
 
 class LifeCriticalError : public NasaError
 {
     public:
-        LifeCriticalError(std::string const &message, std::string const &component = "Unknown") throw();
+        LifeCriticalError(std::string const &message, std::string const &component = "Unknown");
 };
 
 class UserError : public NasaError
 {
     public:
-        UserError(std::string const &message, std::string const &component = "Unknown") throw();
+        UserError(std::string const &message, std::string const &component = "Unknown");
 };
 
 class CommunicationError : public NasaError
@@ -43,5 +42,5 @@ class CommunicationError : public NasaError
     return (i);
 }
     public:
-        CommunicationError(std::string const &message, std::string const &component = "CommunicationDevice") throw();
+        CommunicationError(std::string const &message);
 };
