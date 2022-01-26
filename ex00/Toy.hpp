1028d49ce82759eb46fc6598181f82c99c9eae19
titi0267 <timothe@coniel.com>
2022-01-26 13:55:17 +0100

    finish ex00

---
+++ ex00/Toy.hpp
@@ -0,0 +1,35 @@
+/*
+** EPITECH PROJECT, 2022
+** B-PDG-300-STG-3-1-PDGD11-timothe.coniel
+** File description:
+** Toy
+*/
+
+#include "Picture.hpp"
+#ifndef TOY_HPP_
+#define TOY_HPP_
+
+
+class Toy : public Picture {
+    public:
+        Toy();
+        ~Toy();
+        enum ToyType {
+            BASIC_TOY,
+            ALIEN
+        };
+        Toy(ToyType type, std::string name, const std::string &file);
+        int getType() const;
+        std::string getName() const;
+        void setName(std::string);
+        bool setAscii(const std::string &file);
+        std::string getAscii();
+
+    protected:
+        ToyType _type;
+        std::string _name;
+        Picture _picture;
+    private:
+};
+
+#endif /* !TOY_HPP_ */
