e284c6796f869599dbb314c4cdde7e44b8f79490
titi0267 <timothe@coniel.com>
2022-01-26 21:59:48 +0100

    finish ex04

---
+++ ex04/Toy.cpp
@@ -0,0 +1,82 @@
+/*
+** EPITECH PROJECT, 2022
+** B-PDG-300-STG-3-1-PDGD11-timothe.coniel
+** File description:
+** Toy
+*/
+
+#include <iostream>
+#include <memory>
+
+#include "Toy.hpp"
+#include "Buzz.hpp"
+#include "Woody.hpp"
+#include "Picture.hpp"
+
+Toy::Toy()
+{
+    _type = BASIC_TOY;
+    setName("toy");
+}
+
+Toy::Toy(ToyType type, std::string name, const std::string &file)
+{
+    _type = type;
+    _name = name;
+    _picture.getPictureFromFile(file);
+}
+
+Toy::~Toy()
+{
+}
+
+int Toy::getType() const
+{
+    return _type;
+}
+
+std::string Toy::getName() const
+{
+    return _name;
+}
+
+
+void Toy::setName(std::string name)
+{
+    _name = name;
+}
+
+bool Toy::setAscii(const std::string &file)
+{
+    return _picture.getPictureFromFile(file);
+}
+
+std::string Toy::getAscii() const
+{
+    return _picture.data;
+}
+
+Toy& Toy::operator=(const Toy& toy)
+{
+    _type = toy._type;
+    _name = toy._name;
+    _picture = toy._picture;
+    return (*this);
+}
+
+void Toy::speak(std::string statement)
+{
+    std::cout << getName() << " \"" << statement << "\"" <<  std::endl;
+}
+
+Toy &Toy::operator<<(std::string str)
+{
+    _picture.data = str;
+    return (*this);
+}
+
+std::ostream &operator<<(std::ostream &os, Toy &toy)
+{
+    os << toy.getName() << std::endl << toy.getAscii() << std::endl;
+    return os;
+}
