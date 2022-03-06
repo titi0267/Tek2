b6f87ea0178b2dca7480ef597e2447ee3ab33f03
coniel <timothe@coniel.com>
2022-03-06 18:10:03 +0100

    fix strcasecmp

--- lib/strcasecmp.asm
+++ lib/strcasecmp.asm
@@ -5,27 +5,42 @@ GLOBAL strcasecmp
 strcasecmp:
         XOR     RAX, RAX
         XOR     RCX, RCX
+        XOR     RDX, RDX
         XOR     R10, R10
         XOR     R11, R11
 
 loop:
         MOV     R11B, BYTE[RDI + RCX]
-        MOV     R10B, BYTE[RSI + RCX]
+        MOV     R10B, BYTE[RSI + RDX]
         JMP     is_letter
 
 continue_loop:
         CMP     R11B, R10B
         JNE     bf_end
         CMP     BYTE[RDI + RCX], 0
-        JE      bf_end
-        CMP     BYTE[RSI + RCX], 0
-        JE      bf_end
+        JE      stop_inc_rcx
         INC     RCX
+
+cmp_rsi:
+        CMP     BYTE[RSI + RDX], 0
+        JE      stop_inc_rdx
+        INC     RDX
         JMP     loop
+
+stop_inc_rcx:
+        CMP     BYTE[RSI + RDX], 0
+        JE      bf_end
+        JMP     cmp_rsi
+
+stop_inc_rdx:
+        CMP     BYTE[RDI + RCX], 0
+        JE      bf_end
+        JMP     loop
+
 
 found_maj_al:
         ADD     R11B, 32
-        JMP     continue_loop
+        JMP     is_letter_sec
 
 found_maj_R10B:
         ADD     R10B, 32
@@ -38,8 +53,14 @@ is_letter:
         JL      continue_loop
         CMP     R11B, 90
         JLE     found_maj_al
+
+is_letter_sec:
         CMP     R10B, 90
         JLE     found_maj_R10B
+        JMP     continue_loop
+
+end_str:
+        
 
 bf_end:
         SUB     R11B, R10B
