eb93a23c17cb38e19c1e8268d46e3d38ea854ad1
coniel <timothe@coniel.com>
2022-03-13 18:34:27 +0100

    fix too long functioin

--- src_nm/elf64.c
+++ src_nm/elf64.c
@@ -16,16 +16,12 @@ int my_strcmp(const char *s1, const char *s2)
     c = (strlen(s1) < strlen(s2)) ? b : a;
     for (int i = 0; i != c; i++) {
         if (s1[i] > s2[i]) {
-            if (s1[i] == '@')
-                return (1);
             if (s2[i] == '@')
                 return (-1);
             return (1);
         }
         else if (s1[i] < s2[i]) {
-            if (s1[i] == '@')
-                return (1);
-            if (s2[i] == '@')
+            if (s1[i] == '@' || s2[i] == '@')
                 return (1);
             return (-1);
         }
