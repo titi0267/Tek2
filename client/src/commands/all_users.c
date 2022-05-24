8a156b4a60db63afa089900473eca82f26d7b749
coniel <timothe@coniel.com>
2022-05-24 18:19:57 +0200

    Add: Send info to server from client

--- client/src/commands/all_users.c
+++ client/src/commands/all_users.c
@@ -10,8 +10,12 @@
 
 int c_users(char *buff, client_t *client)
 {
+    message_t msg;
+
     if (buff[0] != '\n')
         return (CMD_ERROR);
     printf("users\n");
+    msg.command = USERS;
+    write(client->socket_fd, &msg, sizeof(message_t));
     return (USERS);
 }
