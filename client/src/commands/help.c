87998f8d2fa87495e200afb7632a76102cb6a394
coniel <timothe@coniel.com>
2022-05-24 03:49:16 +0200

    Add: Shared include with structs to send from cli to serv & vice versa

--- client/src/commands/help.c
+++ client/src/commands/help.c
@@ -7,10 +7,38 @@
 
 #include "../../include/command.h"
 
+void provide_help()
+{
+    printf("    /unsubscribe [\"team_uuid\"] : unsubscribe from a team\n");
+    printf("    /use ?[\"team_uuid\"] ?[\"channel_uuid\"] ?[\"thread_uuid\"] ");
+    printf(": use specify a context team/channel/thread\n");
+    printf("    /create : based on what is being used create the sub ");
+    printf("resource (see below)\n");
+    printf("    /list : based on what is being used list all the sub ");
+    printf("resources (see below)\n");
+    printf("    /info : based on what is being used list the ");
+    printf("current (see below)\n");
+}
+
 int c_help(char *buff)
 {
     if (buff[0] != '\n')
         return (CMD_ERROR);
-    printf("/help\n");
+    printf("Commands alvailable :\n");
+    printf("    /help : List all the commands alvailable\n");
+    printf("    /login [\"Username\"] : set the Username used by client\n");
+    printf("    /logout : disconnect the client from the server\n");
+    printf("    /users : get the list of all users that exist on the domain\n");
+    printf("    /user [\"user_uuid\"] : get information about a user\n");
+    printf("    /send [\"user_uuid”\"] [\"message_body\"] : send a message");
+    printf(" to a user\n");
+    printf("    /messages [\"user_uuid\"] : list all messages exchange");
+    printf(" with an user\n");
+    printf("    /subscribe [\"team_uuid\"] : subscribe to the event of a team");
+    printf(" and its sub directories (enable reception of all events ");
+    printf(" from a team)\n");
+    printf("    /subscribed ?[\"team_uuid\"] : list all subscribed teams or ");
+    printf(" list all users subscribed to a team\n");
+    provide_help();
     return (HELP);
 }
---
+++ include/communication.h
@@ -0,0 +1,21 @@
+/*
+** EPITECH PROJECT, 2022
+** teams
+** File description:
+** communication
+*/
+
+#pragma once
+
+typedef struct user_info_s {
+    int status;
+} user_info_t;
+
+typedef struct team_info_s {
+    int status;
+} team_info_t;
+
+typedef struct com_type_s {
+    int user_info;
+    int team_info;
+} com_type_t;
