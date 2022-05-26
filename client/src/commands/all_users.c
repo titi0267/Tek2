aaf25b0e3e0e50e3773b414530022fed7ec4c965
coniel <timothe@coniel.com>
2022-05-26 23:56:20 +0200

    Bug: user cmd doesn't return the right outpout

--- client/src/commands/all_users.c
+++ client/src/commands/all_users.c
@@ -8,6 +8,19 @@
 #include "../../include/teams.h"
 #include "../../include/command.h"
 
+void get_users(client_t *client)
+{
+    server_get_user_t user;
+    int i = 1;
+
+    while (1) {
+        read(client->socket_fd, &user, sizeof(server_get_user_t));
+        if (user.found == NOT_FOUND)
+            break;
+        client_print_users(user.uid, user.pseudo, user.connected);
+    }
+}
+
 int c_users(char *buff, client_t *client)
 {
     message_t msg;
@@ -17,5 +30,6 @@ int c_users(char *buff, client_t *client)
     printf("users\n");
     msg.command = USERS;
     write(client->socket_fd, &msg, sizeof(message_t));
+    get_users(client);
     return (USERS);
 }
