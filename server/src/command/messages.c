082c18c2118fc44e143473cba7adbaf84aea91ec
ludo <ludo@ludo.ludo>
2022-05-31 14:38:21 +0100

    Fix: messages read good struct

--- server/src/command/messages.c
+++ server/src/command/messages.c
@@ -17,7 +17,7 @@ int open_good_fd(client_list_t *client, cli_messages_t message)
         return (-1);
     sprintf(buff, "./saves/message/conv%d_%d.txt",
     (me <= to_send) ? me : to_send, (me <= to_send) ? to_send : me);
-    return (open(buff, O_WRONLY | O_APPEND | O_CREAT, 0777));
+    return (open(buff, O_RDWR | O_APPEND | O_CREAT, 0777));
 }
 
 void messages(teams_t *server, client_list_t *client)
@@ -31,13 +31,14 @@ void messages(teams_t *server, client_list_t *client)
     read(client->fd, &messages, sizeof(cli_messages_t));
     fd = open_good_fd(client, messages);
     if (fd == -1) {
-        write(fd, &server_message, sizeof(server_message_t));
         write(client->fd, &server_message, sizeof(server_message_t));
         return;
     }
-    while ((read_ret = read(fd, &server_message, sizeof(server_user_t))) != 0
-    && read_ret != -1)
+    while ((read_ret = read(fd, &server_message, sizeof(server_message_t)))
+    != 0 && read_ret != -1) {
+        printf("%d\n", server_message.is_valid);
         write(client->fd, &server_message, sizeof(server_message_t));
+    }
     server_message = get_default_message();
     write(client->fd, &server_message, sizeof(server_message_t));
 }
