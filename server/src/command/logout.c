1a567e5456f1d81dedaa487b509508c7b427cf1b
ludo <ludo@ludo.ludo>
2022-06-01 21:09:25 +0100

    Fix: remove useless unused

--- server/src/command/logout.c
+++ server/src/command/logout.c
@@ -12,7 +12,6 @@ void logout(teams_t *server, client_list_t *client)
     cli_logout_t logout;
     server_user_t user = get_default_user();
 
-    UNUSED(server);
     read(client->fd, &logout, sizeof(cli_logout_t));
     if (strlen(client->uid) != 0) {
         strcpy(user.pseudo, client->pseudo);
