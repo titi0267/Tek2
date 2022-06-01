a1d76e1f6c19d87603298b837a07fe766a89ea11
ludo <ludo@ludo.ludo>
2022-06-01 21:08:27 +0100

    Feat: implement first global sended payload

--- server/src/command/logout.c
+++ server/src/command/logout.c
@@ -10,8 +10,13 @@
 void logout(teams_t *server, client_list_t *client)
 {
     cli_logout_t logout;
+    server_user_t user = get_default_user();
 
     read(client->fd, &logout, sizeof(cli_logout_t));
-    if (strlen(client->uid) != 0)
+    if (strlen(client->uid) != 0) {
+        strcpy(user.pseudo, client->pseudo);
+        strcpy(user.uid, client->uid);
+        send_to_everyone(server, (int)LOGOUT, &user, sizeof(server_user_t));
         server_event_user_logged_out(client->uid);
+    }
 }
