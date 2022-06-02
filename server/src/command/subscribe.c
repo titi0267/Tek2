dbf50d440d1c7a6f731549c6ac17a7a1d730d302
ludo <ludo@ludo.ludo>
2022-06-03 00:13:26 +0100

    Fix: send good sub payload

--- server/src/command/subscribe.c
+++ server/src/command/subscribe.c
@@ -21,6 +21,19 @@ void send_sub_error(client_list_t *client)
     write(client->fd, &subscribe_res, sizeof(server_sub_t));
 }
 
+server_sub_t get_sub_res(client_list_t *client,
+cli_subscribe_t subscribe_payload)
+{
+    server_sub_t sub;
+
+    sub.exist = 1;
+    memset(sub.team_uid, 0, MAX_NAME_LENGTH);
+    memset(sub.user_uuid, 0, MAX_NAME_LENGTH);
+    strcpy(sub.team_uid, subscribe_payload.team_uuid);
+    strcpy(sub.user_uuid, client->uid);
+    return (sub);
+}
+
 server_team_user_t get_team_user(char *pseudo, char *uid)
 {
     server_team_user_t team_user;
@@ -75,13 +88,14 @@ void subscribe(teams_t *server, client_list_t *client)
     read(client->fd, &subscribe_payload, sizeof(cli_subscribe_t));
     fd = get_open_team_users(subscribe_payload);
     if (fd == -1)
-        send_sub_error(client);
+        return (send_sub_error(client));
     if (!check_inactive_user(client, subscribe_payload, fd)) {
         tmp = get_team_user(client->pseudo, client->uid);
         lseek(fd, 0, SEEK_END);
         server_event_user_subscribed(subscribe_payload.team_uuid, client->uid);
         write(fd, &tmp, sizeof(server_team_user_t));
     }
+    subscribe_res = get_sub_res(client, subscribe_payload);
     send_to_everyone(server, (int)SUBSCRIBE, &subscribe_res,
-    sizeof(send_payload_t));
+    sizeof(server_sub_t));
 }
