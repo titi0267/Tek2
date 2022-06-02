8a6815c512641a52f525881ddb2045441aee0b52
coniel <timothe@coniel.com>
2022-06-02 16:39:40 +0200

    Fix: Issue #25

--- client/src/server_reply/logout.c
+++ client/src/server_reply/logout.c
@@ -14,6 +14,5 @@ void r_logout(client_t *client)
     server_user_t usr_logout;
 
     read(client->socket_fd, &usr_logout, sizeof(server_user_t));
-    if (client->user_uuid[0] != 0)
-        client_event_logged_out(usr_logout.uid, usr_logout.pseudo);
+    client_event_logged_out(usr_logout.uid, usr_logout.pseudo);
 }
