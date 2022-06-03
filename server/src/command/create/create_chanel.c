12b13f8c1dec1f1b5f26519048cc90a3ddfed87a
ludo <ludo@ludo.ludo>
2022-06-03 21:53:37 +0100

    Fix: send creator id on channel

--- server/src/command/create/create_chanel.c
+++ server/src/command/create/create_chanel.c
@@ -32,7 +32,8 @@ int error_code)
     write(client->fd, &chanel_info, sizeof(server_create_info_t));
 }
 
-server_create_info_t create_chanel_info(cli_create_t payload, char *id)
+server_create_info_t create_chanel_info(client_list_t *client,
+cli_create_t payload, char *id)
 {
     server_create_info_t chanel_info;
 
@@ -50,10 +51,12 @@ server_create_info_t create_chanel_info(cli_create_t payload, char *id)
     strcpy(chanel_info.team_uuid, payload.team_uuid);
     strcpy(chanel_info.description, payload.description);
     strcpy(chanel_info.channel_uuid, id);
+    strcpy(chanel_info.creator_uuid, client->uid);
     return (chanel_info);
 }
 
-void create_first_chanel(teams_t *server, cli_create_t payload)
+void create_first_chanel(teams_t *server, client_list_t *client,
+cli_create_t payload)
 {
     int fd = 0;
     server_create_info_t chanel_info;
@@ -66,7 +69,7 @@ void create_first_chanel(teams_t *server, cli_create_t payload)
     fd = open(path, O_RDWR | O_CREAT, 0777);
     if (fd == -1)
         return;
-    chanel_info = create_chanel_info(payload, "1");
+    chanel_info = create_chanel_info(client, payload, "1");
     write(fd, &chanel_info, sizeof(server_create_info_t));
     send_to_team(server, &chanel_info, sizeof(server_create_info_t),
     chanel_info.team_uuid);
@@ -74,27 +77,27 @@ void create_first_chanel(teams_t *server, cli_create_t payload)
     chanel_info.channel_uuid, chanel_info.name);
 }
 
-void create_next_chanel(teams_t *server, client_list_t *client,
-cli_create_t payload, char *last_id)
+void create_next_chanel(teams_t *server, client_list_t *cli,
+cli_create_t req, char *last_id)
 {
     char *path = malloc(100);
     server_create_info_t chanel_info;
     int fd = 0;
 
-    sprintf(path, "./saves/teams/t_%d/c_%d", atoi(payload.team_uuid)
+    sprintf(path, "./saves/teams/t_%d/c_%d", atoi(req.team_uuid)
     , atoi(last_id) + 1);
     mkdir(path, 0777);
     sprintf(path, "./saves/teams/t_%d/c_%d/channel_info.txt"
-    , atoi(payload.team_uuid), atoi(last_id) + 1);
+    , atoi(req.team_uuid), atoi(last_id) + 1);
     fd = open(path, O_RDWR | O_CREAT, 0777);
     if (fd == -1)
         return;
-    chanel_info = create_chanel_info(payload, increment_str(atoi(last_id)));
+    chanel_info = create_chanel_info(cli, req, increment_str(atoi(last_id)));
     write(fd, &chanel_info, sizeof(server_create_info_t));
     send_to_team(server, &chanel_info, sizeof(server_create_info_t),
     chanel_info.team_uuid);
     server_event_team_created(chanel_info.team_uuid,
-    chanel_info.name, client->uid);
+    chanel_info.name, cli->uid);
     server_event_channel_created(chanel_info.team_uuid,
     chanel_info.channel_uuid, chanel_info.name);
 }
@@ -120,6 +123,6 @@ cli_create_t payload)
         (strncmp(ep->d_name, "c_", 2) == 0 && is_bigger_id(ep->d_name, buff))
         ?  buff = ep->d_name : 0;
     if (strlen(buff++) == 0)
-        return (create_first_chanel(server, payload));
+        return (create_first_chanel(server, client, payload));
     create_next_chanel(server, client, payload, ++buff);
 }
