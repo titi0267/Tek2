655335a0642e5cf6daeb256d014ef2dd4f06db98
coniel <timothe@coniel.com>
2022-06-03 03:00:18 +0200

    Fix: Unsub print issue

--- client/src/server_reply/unsubscribe.c
+++ client/src/server_reply/unsubscribe.c
@@ -14,8 +14,10 @@ void r_unsubscribe(client_t *client)
     server_unsub_t unsub;
 
     read(client->socket_fd, &unsub, sizeof(server_unsub_t));
-    if (unsub.valid == 0)
-        ;//client_error_unknown_team(unsub.team_uid);
-    //client_print_unsubscribed(unsub.uid, unsub.team_uid);
+    if (unsub.valid == 0) {
+        client_error_unknown_team(unsub.team_uid);
+        return;
+    }
+    client_print_unsubscribed(unsub.user_uuid, unsub.team_uid);
     UNUSED(client);
 }
